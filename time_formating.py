abb5ba1d1e29276e615b1e18902d29175c31626b
kevin omar <kaliomar89@yahoo.com>
2018-03-27 18:39:13 +0200

    adding notes in time_formating

--- time_formating.py
+++ time_formating.py
@@ -1,16 +1,22 @@
 def readable(seconds):
-    minute = 60
-    hour = 3600
-    day = 86400
-    year = 31536000
-    time = []
-    if seconds < 0:
+    ''' this function takes time in seconds and return it in a human readable way '''
+    minute = 60     # seconds
+    hour = 3600     # seconds
+    day = 86400     # seconds
+    year = 31536000  # seconds
+    time = []  # time =[years_count, years_format,
+    ###        days_count, days_format,
+    # hours_count,hours_format,
+    ###        minutes_count, minutess_format,
+    # seconds_count, seconds_format,]
+    if seconds < 0:  # against negative numbers
         stru = "Error"
-    if seconds == 0:
+    if seconds == 0:  # against zero
         stru = "now"
     if seconds > 0:
         years = seconds // year
         time.append(years)
+        # format
         if years == 1:
             form_y = "year"
             time.append(form_y)
@@ -68,15 +74,21 @@ def readable(seconds):
         if seconds == 0:
             form_s = "0"
             time.append(form_s)
-        printout = []
+        printout = []  # list to neglect parts which value == 0
         for i in range(0, len(time), 2):
             if time[i] > 0:
                 printout.append(time[i])
                 printout.append(time[i + 1])
-        collective = []
+        collective = []  # list to join every part with it's format e.g. [10, "years"] >>> '10 years'
         for i in range(0, (len(printout)), 2):
             member = str(printout[i]) + " " + printout[i + 1]
             collective.append(member)
+            # next part just here for change the seperation of the last part of time from ", " to " and "
+            # for example:
+            # 10 days, 2 hours , 5 minutes >>>> 10 days, 2 hours and 5 minutes
+            # stru is a function to convert list to string
+            # for example:
+            # ['10 days', '2 hours', '41 minutes', '1 second'] >>> 10 days, 2 hours, 41 minutes and 1 second
         for i in range(0, (len(collective) + 1)):
             if collective[-1] != collective[0]:
                 stru = ', '.join(map(str, collective[0:-1])) + " " + "and" + " " + str(printout[-2]) + " " + printout[-1]
