ff6b7aa443c67ea3957ae00b26a64ad69da5359c
kaliomar <kaliomar89@yahoo.com>
2019-12-26 17:25:27 +0200

    handling error

--- QuraanPartStart.py
+++ QuraanPartStart.py
@@ -1,20 +1,25 @@
+#!/usr/bin/env python
 def pageforQuran():
   print("""Usage: Choose a number between 1-30 to get the page.
-       For exit this programe press 'q'. """)
+       For exit this programe press 0.""")
 
-  part = []
-  while part != 'q':
-    part = (input('Write a number of part: '))
-    if part < 1 or part > 30:
-      print('Number must be 1-30')
-      continue
+  try:
+    part = []
+    while part != 0:
+      part = int(input('Write a number of part: '))
+      if part < 1 or part > 30:
+        print('Number must be 1-30')
+        # continue
+      else:
+        n = 2 * (part - 1)
+        page = str(n) + '2'
+        print('Start on page: {}'.format(page))
+        # continue
     else:
-      n = 2 * (part - 1)
-      page = str(n) + '2'
-      print('Start on page:{}'.format(page))
-      continue
-  else:
-    print('Assalamo Alikom!')
+      print('Assalamo Alikom!')
+
+  except ValueError as err:
+    print('Unvalid value - Enter a number')
 
 
 pageforQuran()
